8f727b1796d596f2e79d4b8f8baca1311547303f
davinerd <anathema@anche.no>
2019-07-02 10:10:40 +0200

    added policy json in output

--- output.tf
+++ output.tf
@@ -10,3 +10,10 @@ output "s3_bucket_arn" {
   value = local.s3_bucket_arn
 }
 
+output "s3_policy_json" {
+  value = data.aws_iam_policy_document.s3_access_policy.json
+}
+
+output "s3_replica_policy_json" {
+  value = data.aws_iam_policy_document.replica_access_policy.json
+}
